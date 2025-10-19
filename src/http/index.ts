5ccdac6a6582c7b0931a172d1ad27a872cf4393c
Burkalev Semyon <gladodinochka@mail.ru>
2025-10-19 23:53:38 +0300

    try fix

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5050/api';
+const API_URL = 'https://task-scheduler-production-d118.up.railway.app/api';
 
 const $api = axios.create({
     withCredentials: true,
